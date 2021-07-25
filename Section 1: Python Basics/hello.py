dbed33c0a4ec9c4e99f8b29316168d18e006d568
Mohammed Asir Shahid <asirshahid@protonmail.com>
2021-07-25 18:43:28 -0400

    Created hello.py file

---
+++ Section 1: Python Basics/hello.py
@@ -0,0 +1,12 @@
+name="Asir"
+age="22"
+print("Hello World")
+
+print("What is your name?")
+
+print("It is good to meet you, {}".format(name))
+# Alternatively, we could concatenate
+# print("It is good to meet you, " + name)
+print("The length of your name is: {}".format(str(len(name))))
+
+print("You will be {} in a year".format(str(int(age)+1)))
