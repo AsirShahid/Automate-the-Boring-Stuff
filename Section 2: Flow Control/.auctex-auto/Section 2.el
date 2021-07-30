929ae462be39ef65a88bee971d13bba854c66bb6
Mohammed Asir Shahid <asirshahid@protonmail.com>
2021-07-30 13:21:25 -0400

    Finished For Loops

--- Section 2: Flow Control/.auctex-auto/Section 2.el
+++ Section 2: Flow Control/.auctex-auto/Section 2.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,10 +31,11 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org0bca62c"
-    "sec:orgfb302c0"
-    "sec:org2165e4f"
-    "sec:org3966279"
-    "sec:orgf72386b"))
+    "sec:org385e513"
+    "sec:org3aaefb4"
+    "sec:orged04b11"
+    "sec:org7d61a55"
+    "sec:org36d7140"
+    "sec:orge727d62"))
  :latex)
 
