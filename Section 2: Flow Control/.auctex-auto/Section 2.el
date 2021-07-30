dfd1c1ded12b583ad2ee53b262b1e662dba3a7e5
Mohammed Asir Shahid <asirshahid@protonmail.com>
2021-07-29 22:21:12 -0400

    Finished If, Else, and Elif Statements

--- Section 2: Flow Control/.auctex-auto/Section 2.el
+++ Section 2: Flow Control/.auctex-auto/Section 2.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,8 +31,9 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org28f27a3"
-    "sec:orgb64b3b4"
-    "sec:orga6267ac"))
+    "sec:orgc0b0ef5"
+    "sec:org3481943"
+    "sec:org5d8eaa8"
+    "sec:orgcd7ca64"))
  :latex)
 
