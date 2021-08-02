86c625469b7e02a1b1001de43b19cc217a8f46fb
Mohammed Asir Shahid <asirshahid@protonmail.com>
2021-08-02 14:10:33 -0400

    Added Section 6 mp4s

---
+++ .auctex-auto/Final.el
@@ -0,0 +1,43 @@
+(TeX-add-style-hook
+ "Final"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org5d0538b"
+    "sec:org811f7c4"
+    "sec:orgc829b6a"
+    "sec:org18ca2e9"
+    "sec:org205ae83"
+    "sec:org9ff0173"
+    "sec:org6dcd4c5"
+    "sec:orgb096098"))
+ :latex)
+
