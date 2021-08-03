07fcaf490ea475987fdc15fbb491caaffbc44185
Mohammed Asir Shahid <asirshahid@protonmail.com>
2021-08-03 19:34:24 -0400

    Finished Section 9

--- Section 9: Running Programs from the Command Line/.auctex-auto/Section 9.el
+++ Section 9: Running Programs from the Command Line/.auctex-auto/Section 9.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,6 +31,8 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org4d7157c"))
+    "sec:org8e0ce82"
+    "sec:orgef1f61d"
+    "sec:org23e5d60"))
  :latex)
 
--- Section 9: Running Programs from the Command Line/Section 9.md
+++ Section 9: Running Programs from the Command Line/Section 9.md
@@ -1,17 +1,33 @@
 
 # Table of Contents
 
-1.  [Launching Python Programs from Outside IDLE](#org429dc8a)
+1.  [Launching Python Programs from Outside IDLE](#orgedccc46)
+    1.  [Shebang Line](#org19f556b)
+    2.  [The Terminal](#org7f44a64)
 
 
 
-<a id="org429dc8a"></a>
+<a id="orgedccc46"></a>
 
 # Launching Python Programs from Outside IDLE
 
-Opening your IDE, then opening the program, and then
+Opening your IDE, then opening the program, and then running it is time consuming and not convenient. You can instead run it from the terminal, but the way to do this differs based on your operating system.
 
-\*\*
 
-    #
+<a id="org19f556b"></a>
+
+## Shebang Line
+
+The shebang line should be the first line in your Python programs. In Linux it is
+
+    #! /usr/bin/python3
+
+
+<a id="org7f44a64"></a>
+
+## The Terminal
+
+Next you must open up a terminal, run python and then give the path to the file you want to run.
+
+The rest of the lesson is Windows specific.
 
